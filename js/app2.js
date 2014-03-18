c1df3510c1f6193fac1bf44a383f8592dfd5f566
arsood <arsood@gmail.com>
2014-03-18 14:53:12 -0700

    Quick change

---
+++ js/app2.js
@@ -0,0 +1,13 @@
+/*To use this file, simply remove the script link from the html file called app.js. This file will serve as the calls to the Hangman library.*/
+
+//Write a function called getUserWords
+
+//Define a new variable by assigning a prompt to it asking the user 'What would you like your partner to guess?'
+
+//Check that the prompt answer is not empty. If it is empty, call the function again
+
+//If the prompt answer is NOT empty, call the function HANG.saveBlock passing in the prompt answer as a parameter
+
+//Make sure to call the function OUTSIDE of the function
+
+//Call the function HANG.getGuess() with a 100 millisecond timeout
